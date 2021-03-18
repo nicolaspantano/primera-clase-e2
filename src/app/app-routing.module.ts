6545d9e652edba8d65a7d5f08edfd09b390aa678
nicolaspantano <nicolaspantano_757@hotmail.com>
2021-03-18 19:04:43 -0300

    Clase 1 Ejercicio 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'bienvenidos',component:BienvenidoComponent},
+  {path:'login',component:LoginComponent},
+  {path:'error',component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
